f0174936b85839400272c7db799551056bd3c0fa
NullExc <peter.zemianek@gmail.com>
2021-02-22 23:43:16 +0100

    Fundamental mechanics
    Basic movement, jump and climing on ladder
    
    Simple animations and animator state machine

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private PlayerControls playerControls;
+    private Rigidbody2D rigidBody;
+    private Collider2D collider;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private float gravityScaleAtStart;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float climbSpeed;
+
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private LayerMask climbLadder;
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rigidBody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControls.Land.Jump.performed += _ => Jump();
+        gravityScaleAtStart = rigidBody.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        ClimbLadder();
+    }
+
+    private void Run()
+    {
+        float movementInput = playerControls.Land.Move.ReadValue<float>();
+
+        Vector3 currentPosition = transform.position;
+        currentPosition.x += movementInput * speed * Time.deltaTime;
+        transform.position = currentPosition;
+
+        if (movementInput == 0)
+        {
+            animator.SetBool("Running", false);
+        } else
+        {
+            animator.SetBool("Running", true);
+        }
+    }
+
+    private void ClimbLadder()
+    {
+        if (collider.IsTouchingLayers(climbLadder))
+        {
+            float climbInput = playerControls.Land.ClimbLadder.ReadValue<float>();
+            Vector2 rigidBodyVelocity = new Vector2(rigidBody.velocity.x, climbInput * climbSpeed);
+            rigidBody.velocity = rigidBodyVelocity;
+            rigidBody.gravityScale = 0f;
+
+            if (climbInput == 0)
+            {
+                animator.SetBool("Climbing", false);
+            }
+            else
+            {
+                animator.SetBool("Climbing", true);
+            }
+        } 
+        else
+        {
+            animator.SetBool("Climbing", false);
+            rigidBody.gravityScale = gravityScaleAtStart;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        float movementInput = playerControls.Land.Move.ReadValue<float>();
+        if (movementInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        } 
+        else if (movementInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0, jumpSpeed);
+            rigidBody.velocity = jumpVelocityToAdd;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return collider.IsTouchingLayers(ground);
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,234 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Land"",
+            ""id"": ""c5c0db26-579a-4cc5-9598-9862fdd8e285"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""8877fc6c-d502-493a-ab33-5ba7370afa73"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""c1b0910c-9113-4b8d-ab75-caf859fdfb89"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""ClimbLadder"",
+                    ""type"": ""Button"",
+                    ""id"": ""8b5c3a3b-95be-432e-b4fb-1d5fff910f91"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Sideways"",
+                    ""id"": ""62385d59-abdf-4575-bb62-982f87d7ab8d"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""95c77e19-e9da-4b4d-91a5-5fb1696d05d7"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""71e7861d-33e7-4096-90c5-5f655694ea07"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a1c34f2c-af1e-4606-b219-f64559f9c22e"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""b3503160-055d-4ba4-ba87-0648d77ea699"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ClimbLadder"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""4a9404d5-5efd-4765-9d65-c152e7b9f411"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ClimbLadder"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""bf819a4e-3e5e-43e8-b5dd-57413457a732"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ClimbLadder"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Land
+        m_Land = asset.FindActionMap("Land", throwIfNotFound: true);
+        m_Land_Move = m_Land.FindAction("Move", throwIfNotFound: true);
+        m_Land_Jump = m_Land.FindAction("Jump", throwIfNotFound: true);
+        m_Land_ClimbLadder = m_Land.FindAction("ClimbLadder", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Land
+    private readonly InputActionMap m_Land;
+    private ILandActions m_LandActionsCallbackInterface;
+    private readonly InputAction m_Land_Move;
+    private readonly InputAction m_Land_Jump;
+    private readonly InputAction m_Land_ClimbLadder;
+    public struct LandActions
+    {
+        private @PlayerControls m_Wrapper;
+        public LandActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Land_Move;
+        public InputAction @Jump => m_Wrapper.m_Land_Jump;
+        public InputAction @ClimbLadder => m_Wrapper.m_Land_ClimbLadder;
+        public InputActionMap Get() { return m_Wrapper.m_Land; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(LandActions set) { return set.Get(); }
+        public void SetCallbacks(ILandActions instance)
+        {
+            if (m_Wrapper.m_LandActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @ClimbLadder.started -= m_Wrapper.m_LandActionsCallbackInterface.OnClimbLadder;
+                @ClimbLadder.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnClimbLadder;
+                @ClimbLadder.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnClimbLadder;
+            }
+            m_Wrapper.m_LandActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @ClimbLadder.started += instance.OnClimbLadder;
+                @ClimbLadder.performed += instance.OnClimbLadder;
+                @ClimbLadder.canceled += instance.OnClimbLadder;
+            }
+        }
+    }
+    public LandActions @Land => new LandActions(this);
+    public interface ILandActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnClimbLadder(InputAction.CallbackContext context);
+    }
+}
